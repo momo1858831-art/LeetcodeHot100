dde5b0e0198bf206cdb05348a9c386cab76da8ec
momo1858831-art <momo1858831@gmail.com>
2026-09-13 17:02:08 +0800

    移动零

---
+++ 双指针/移动零.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]){
+                nums[j++]=nums[i];
+            }
+        }
+        for(int i=j;i<nums.size();i++){
+            nums[i]=0;
+        }
+    }
+};
