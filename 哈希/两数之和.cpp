1434edb8944db9b0562f9db41e1f07a219b9b2ce
momo1858831-art <momo1858831@gmail.com>
2026-09-13 14:22:38 +0800

    两数之和

---
+++ 哈希/两数之和.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+class Solution{
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>map;
+        vector<int>res;
+        for(int i=0;i<nums.size();i++){
+            if(map.find(target-nums[i])!=map.end()){
+                res.push_back(map[target-nums[i]]);
+                res.push_back(i);
+                return res;
+            }
+            else{
+                map[nums[i]]=i;
+            }
+        }
+        return res;
+    }
+};
+
